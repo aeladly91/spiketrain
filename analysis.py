d09433a535537a095d47eeab35690371a709ac46
ezshen <ezshen@stanford.edu>
2019-03-12 20:04:30 -0700

    data updates

--- analysis.py
+++ analysis.py
@@ -1,5 +1,8 @@
 from helper import *
+import scipy.io as sio
 
-data = load_and_shape('data/spikeTimes_1980_sec.npy')
-plot_events(data, 50)
-print_gap_stats(data)
+data = load_and_shape('data/spikeTimes.npy')
+sio.savemat('data/spikeTimes.mat', {'data': data})
+
+# plot_events(data, 50)
+# print_gap_stats(data)
